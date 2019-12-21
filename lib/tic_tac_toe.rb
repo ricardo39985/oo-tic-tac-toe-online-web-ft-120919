a362644d1e99569869670c9acf1a66b3e7d2efba
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-21 22:25:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,4 +87,28 @@ class TicTacToe
       return false
     end
   end
+  
+ def winner
+   index = []
+   index = won?
+   if index == false
+     return nil
+   else
+     if @board[index[0]] == "X"
+       return "X"
+     else
+       return "O"
+     end
+   end
+ end
+ def play
+ until over? == true
+   turn
+ end
+ if won?
+   puts "Congratulations #{winner}!"
+ elsif draw?
+   puts "Cat's Game!"
+ end
+ end
 end
